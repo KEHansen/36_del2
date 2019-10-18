caee85e3f8bab394e4a532e9ad99c698ba7ed7df
Kristoffer <ktis10@hotmail.com>
2019-10-18 17:27:38 +0200

    startet på player og Dice(kopieret for vores del 1

--- src/Klasser/Player.java
+++ src/Klasser/Player.java
@@ -1,4 +1,16 @@
 package Klasser;
 
 public class Player {
+   private String name;
+   private int score;
+   private boolean win;
+
+public Player (String name, int score, boolean win) {
+
+this.name = name;
+this.score = score;
+this.win = win;
+}
+
+
 }
