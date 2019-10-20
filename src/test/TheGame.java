c5f53b8421f8416e9c260b5f5bf91ac6ce68fb46
Kristoffer <ktis10@hotmail.com>
2019-10-20 15:21:10 +0200

    tilføjet score opsamler

--- src/test/TheGame.java
+++ src/test/TheGame.java
@@ -35,32 +35,32 @@ public class TheGame {
                 case 1:
                     System.out.println("(BACK TO HOME TOWN)");
                     System.out.println( "it's dangerous to go alone, here take 100");
-                    System.out.println(player1.addMoney(+100));
+                    System.out.println(player1.getName() + "you have: " + player1.addMoney(+100) + "points");
                     break;
                 case 2:
                     System.out.println("(TOWER)");
                     System.out.println( "you saved the princess, she gave you 250");
-                    System.out.println(player1.addMoney(+250));
+                    System.out.println(player1.getName() + "you have: " + player1.addMoney(+250) + "points");
                 case 3:
                     System.out.println("(CRATER)");
                     System.out.println("you nearly fell down the crater, only some money felt down");
                     System.out.println("you lost 100");
-                    System.out.println(player1.addMoney(-100));
+                    System.out.println(player1.getName() + "you have: " + player1.addMoney(-100) + "points");
                 case 4:
                     System.out.println("(PALACE GATE)");
                     System.out.println("you sells some off you stuff to the prince");
                     System.out.println("you earned 100");
-                    System.out.println(player1.addMoney(+100));
+                    System.out.println(player1.getName() + "you have: " + player1.addMoney(+100) + "points");
                 case 5:
                     System.out.println("(COLD DESERT)");
                     System.out.println("you´re about to head into th cold Desert. So you buy a scarf");
                     System.out.println("you lose 20");
-                    System.out.println(player1.addMoney(-20));
+                    System.out.println(player1.getName() + "you have: " + player1.addMoney(-20) + "points");
                 case 6:
                     System.out.println("(WALLED CITY");
                     System.out.println("you sell a rare wall to a merchant");
                     System.out.println("you earn 180");
-                    System.out.println(player1.addMoney(180));
+                    System.out.println(player1.getName() + "you have: " + player1.addMoney(+180) + "points");
                 case 7:
                     System.out.println("(MONASTERY)");
                     System.out.println("you just went by, nothing happened");
@@ -68,36 +68,34 @@ public class TheGame {
                     System.out.println("(BLACK CAVE)");
                     System.out.println("you dropped some money on your way out, never to be found again");
                     System.out.println("you lost 70");
-                    System.out.println(player1.addMoney(-70));
+                    System.out.println(player1.getName() + "you have: " + player1.addMoney(-70) + "points");
                 case 9:
                     System.out.println("(HUTS IN THE MOUNTAIN)");
                     System.out.println("you show the villages memes. They love it and give you a present");
                     System.out.println("you earn 60");
-                    System.out.println(player1.addMoney(60));
+                    System.out.println(player1.getName() + "you have: " + player1.addMoney(+60) + "points");
                 case 10:
                     System.out.println("(THE WEREWOLF-WALL)");
                     System.out.println("you ran in fear for a hairy beast");
                     System.out.println("you lose 80 but ran so fast to get a ekstra turn");
-                    System.out.println(player1.addMoney(-80));
+                    System.out.println(player1.getName() + "you have: " + player1.addMoney(-80) + "points");
+                    //slå et slag igen
                 case 11:
                     System.out.println("(THE PIT)");
                     System.out.println("you throw some money to check how deep the pit is");
                     System.out.println("you lose -50");
-                    System.out.println(player1.addMoney(-50));
+                    System.out.println(player1.getName() + "you have: " + player1.addMoney(-50) + "points");
                 case 12:
                     System.out.println("(GOLDMINE)");
                     System.out.println("you struck GOOOOLD!!!");
                     System.out.println("ah yea alot of money, you earn 650!");
-                    System.out.println(player1.addMoney(650));
+                    System.out.println(player1.getName() + "you have: " + player1.addMoney(+650) + "points");
 
                 default:
                     if (boardplace > MAX) {
                         return case 1;
 
                     }
-                    else if (win == true) {
-                        Player + "Won the game with" + money
-                        break;
                     }
 
 
