567878fb9d00ca31f8b9432eda655b3e9a964156
Simon <fridolf.lindhard@gmail.com>
2019-10-25 11:48:28 +0200

    Kode er indsat i respektive klasser (objektorienteret)

--- src/Klasser/Game.java
+++
@@ -1,4 +0,0 @@
-package Klasser;
-
-public class Game {
-}
--- src/klasser/Output.java
+++ src/klasser/Output.java
@@ -1,103 +1,63 @@
-package test;
-
-import Klasser.Dice;
-import Klasser.Player;
+package klasser;
 
 import java.util.Scanner;
 
-//----------------------------------------------------------------------------------------------------------------------
-
-public class TheGame {
+public class Output {
     private static final int MAX_MONEY = 3000;          // maks for at vinde spillet
     public static Dice d1 = new Dice();                 //Laver et objekt ved navn d1 ud fra dice klassen
     public static Dice d2 = new Dice();                 //Laver et objekt ved navn d2 ud fra dice klassen
-    public static int roll1, roll2, counter = 0, counter1 = 0, counter2 = 0;
     public static Scanner key = new Scanner(System.in); // en scanner
-
     public static Player player1 = new Player();        // navnet som bliver holdt på spiller 1
     public static Player player2 = new Player();        // navnet som bliver holdt på spiller 2
 
 
     //------------------------------------------------------------------------------------------------------------------
 
-    public static void main(String[] args) {
 
-        // play intro, som ses i bunden.
-        playStart();
+    //INTRO dialog
 
-        //--------------------------------------------------------------------------------------------------------------
+    public static void playStart() {
 
-        // terning kasten for player 1 og player 2
-
-        // hvis det er under 3000 som er MAX_MONEY så skal den forsæt med dette
-        while ((player1.getMoney() < MAX_MONEY) && (player2.getMoney() < MAX_MONEY)) {
-
-            //----------------------------------------------------------------------------------------------------------
-
-            // spiller 1
-
-            // kast terningerne
-            System.out.println("____________________________________________________________________________________ ");
-            System.out.println("(" + player1.getName() + "!" + " " + "click enter to throw the die)");
-            key.nextLine();
-
-            //Slår med første terning ved at bruge roll funktionen i Dice klassen for både objektet d1 og d2
-            roll1 = d1.roll();
-            roll2 = d2.roll();
-            counter = roll1 + roll2;
+        System.out.println("WELCOME PLAYERS!");
+        System.out.println("TO A WORLD OF ADVENTURE");
+        System.out.println("BEFORE WE START, LETS ME KNOW YOUR NAMES");
+        System.out.println(" ");
+        System.out.println("Player 1, enter your name:");
+        String p1 = key.nextLine();
+        player1.setName(p1);
 
 
-            System.out.println("you roll: " + counter);
-            System.out.println(" ");
+        System.out.println(player1.getName() + " " + "I REALLY LIKE THAT NAME!");
+        System.out.println("WHAT ABOUT YOU, WHAT IS YOUR NAME?");
+        System.out.println(" ");
+        System.out.println("Player 2, enter your name:");
+        String p2 = key.nextLine();
+        player2.setName(p2);
 
-            counter1 = counter1 + counter;
-            if (counter1 > 12)
-                counter1 = counter1 % 12;
+        System.out.println(player2.getName() + " " + "WHAT A COOL NAME!");
+        System.out.println(" ");
+        System.out.println("BEFORE WE START, I HAVE A GIFT FOR BOTH OF YOU");
+        System.out.println(" ");
+        System.out.println("(click enter to get your gifts) ");
+        key.nextLine();
+        System.out.println("-  -  -  -  -  -  -  -  -  -  -  -  -  ");
+        System.out.println("you both reseive 1000 start points!");
+        System.out.println("-  -  -  -  -  -  -  -  -  -  -  -  -  ");
+        System.out.println(" ");
+        System.out.println("lET US DIVE INTO A WORLD FULL OF ADVENTURE!");
+        System.out.println("____________________________________________________________________________________ ");
+        System.out.println(" ");
+        System.out.println("(click enter to start game) ");
+        key.nextLine();
 
-            // player1 bliver sendt ned i boardField som er vores case, længere nede.
-
-            boardFields(counter1, player1);
-
-            //----------------------------------------------------------------------------------------------------------
-
-            // spiller 2
-
-            System.out.println("____________________________________________________________________________________ ");
-            System.out.println("(" + player2.getName() + "!" + " " + "click enter to throw the die)");
-            key.nextLine();
-
-            //Slår med første terning ved at bruge roll funktionen i Dice klassen for både objektet d1 og d2
-            roll1 = d1.roll();
-            roll2 = d2.roll();
-            counter = roll1 + roll2;
-
-            System.out.println("you roll: " + counter);
-            System.out.println(" ");
-
-            counter2 = counter2 + counter;
-            if (counter2 > 12)
-                counter2 = counter2 % 12;
-
-            // player1 bliver sendt ned i boardField som er vores case, længere nede.
-            boardFields(counter2, player2);
-
-            //----------------------------------------------------------------------------------------------------------
-        }
-
-        // win metoden, som står i bunden
-
-        // vinder for spiller 1
-        Win(player1);
-
-        // vinder for spiller 2
-        Win(player2);
     }
 
     //------------------------------------------------------------------------------------------------------------------
 
-    // Selve bræt felterne
+    // Selve brætfelterne
 
     public static void boardFields(int counter, Player player) {
+
         switch (counter) {
             case 1:
                 player.addMoney(+100);
@@ -196,13 +156,10 @@ public class TheGame {
                 System.out.println(player.getName() + " " + "you have: " + player.getMoney() + "points");
                 System.out.println(" ");
                 System.out.println("(click enter to roll again!)");
-                //-------------------------------------------------------------------------------------------------
                 // ekstra tur
                 key.nextLine();
-                roll1 = d1.roll();
-                roll2 = d2.roll();
-                counter = roll1 + roll2;
-
+                counter = counter + d1.roll() + d2.roll();
+                counter = counter % 12;
                 System.out.println("you roll: " + counter);
             case 11:
                 player.addMoney(-50);
@@ -230,50 +187,10 @@ public class TheGame {
 
     //------------------------------------------------------------------------------------------------------------------
 
-    //INTRO dialog metode
-
-    public static void playStart() {
-
-        System.out.println("WELCOME PLAYERS!");
-        System.out.println("TO A WORLD OF ADVENTURE");
-        System.out.println("BEFORE WE START, LETS ME KNOW YOUR NAMES");
-        System.out.println(" ");
-        System.out.println("Player 1, enter your name:");
-        String p1 = key.nextLine();
-        player1.setName(p1);
-
-
-        System.out.println(player1.getName() + " " + "I REALLY LIKE THAT NAME!");
-        System.out.println("WHAT ABOUT YOU, WHAT IS YOUR NAME?");
-        System.out.println(" ");
-        System.out.println("Player 2, enter your name:");
-        String p2 = key.nextLine();
-        player2.setName(p2);
-
-        System.out.println(player2.getName() + " " + "WHAT A COOL NAME!");
-        System.out.println(" ");
-        System.out.println("BEFORE WE START, I HAVE A GIFT FOR BOTH OF YOU");
-        System.out.println(" ");
-        System.out.println("(click enter to get your gifts) ");
-        key.nextLine();
-        System.out.println("-  -  -  -  -  -  -  -  -  -  -  -  -  ");
-        System.out.println("you both reseive 1000 start points!");
-        System.out.println("-  -  -  -  -  -  -  -  -  -  -  -  -  ");
-        System.out.println(" ");
-        System.out.println("lET US DIVE INTO A WORLD FULL OF ADVENTURE!");
-        System.out.println("____________________________________________________________________________________ ");
-        System.out.println(" ");
-        System.out.println("(click enter to start game) ");
-        key.nextLine();
-
-        //------------------------------------------------------------------------------
-    }
-
-    //------------------------------------------------------------------------------------------------------------------
-
     //Vinder metode
 
     public static void Win(Player player) {
+
         // hvis de vinder, køre den dette igennem. Det er kun, hvis de kommer over 3000 point
         if (player.getMoney() > MAX_MONEY) {
             System.out.println("____________________________________________________________________________________ ");
@@ -283,3 +200,4 @@ public class TheGame {
         }
     }
 }
+    //------------------------------------------------------------------------------------------------------------------
