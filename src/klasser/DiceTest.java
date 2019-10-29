c46e8a127e3cda33f94e836943ed8478b7753407
Karl Emil <kehansen3@hotmail.dk>
2019-10-29 10:36:10 +0100

    Sandsynlighedstest for terning.

--- src/klasser/DiceTest.java
+++ src/klasser/DiceTest.java
@@ -5,13 +5,22 @@ import static org.junit.Assert.*;
 public class DiceTest {
 
     @org.junit.Test
-    public void roll() {
+    public void rollStatistics() {
         Dice dice1 = new Dice();
-        assertEquals(2, dice1.roll(), 6);
+        int antalSlag = 100000;
+        float[] arr = new float[6];
+        int t = 0;
+        for (int i = 0; i < antalSlag; i++) {
+            t = dice1.roll() - 1;
+            arr[t] = arr[t] + 1;
+        }
+        for (int i = 0; i < 6; i++) {
+            assertEquals(16.67, arr[i] / antalSlag * 100, 4);
+        }
     }
 
     @org.junit.Test
-    public void rollOutside() {
+    public void roll() {
         Dice dice1 = new Dice();
         for (int i = 0; i < 1000000; i++) {
             assertEquals(3.5, dice1.roll(), 2.5);
